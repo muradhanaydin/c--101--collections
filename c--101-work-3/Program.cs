48d02c62f95f5c4e69322b0c2704c085239b8398
Muradhan Aydın <52311599+muradhanaydin@users.noreply.github.com>
2023-01-01 01:41:43 +0300

    Work 3 Added

---
+++ c--101-work-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System; 
+using System.Collections;
+
+namespace Work1{
+    internal class Program{
+        public static void Main(string[] args){
+            char[] vowelsPath = {'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü'};
+            List<char> vowelsInText = new List<char>();
+            string inp = Console.ReadLine();
+
+            foreach(char c in inp){
+                if(vowelsPath.Contains(c)){
+                    vowelsInText.Add(c);
+                }
+            }
+            vowelsInText.Sort();
+            printElementsInArray<char>(vowelsInText);
+        }
+        public static void printElementsInArray<T>(List<T> arr){
+            foreach(var value in arr){
+                Console.Write($"{value} ");
+            }
+        }
+    }
+}
